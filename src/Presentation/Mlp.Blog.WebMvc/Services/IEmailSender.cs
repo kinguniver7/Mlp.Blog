f35061ea1a59908413ebf6701836d067b9a47679
malpinsoft <malpinsoft@gmail.com>
2016-12-01 20:54:56 +0200

    start

---
+++ src/Presentation/Mlp.Blog.WebMvc/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mlp.Blog.WebMvc.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
